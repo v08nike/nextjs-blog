450c165dce45461e0aef9f058d4c51a10e307b9e
v08nike <teddan817@gmail.com>
2022-10-24 06:17:04 -0700

    static resource managing practice

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,18 @@
 import Link from 'next/link' // client side navigation
+import Image from 'next/image'
 
 export default function FirstPost() {
   return <>
     <h1>First Post</h1>
     <h2>
       <Link href="/">Back to home</Link>
+      {/* <img src="/images/Rabbit.png" alt='Rabbit' /> */}
+      <Image 
+        src="/images/Rabbit.png"
+        height={144}
+        width={144}
+        alt="Rabbit"
+        />
     </h2>
   </>;
 }
