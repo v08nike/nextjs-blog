45507b1c96a4b195c63346d400e08154a85e5c66
v08nike <teddan817@gmail.com>
2022-11-08 11:04:32 -0800

    pre-rendering and data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
+  console.log(allPostsData);
   return (
     <Layout home>
       <Head>
@@ -11,10 +13,40 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>[Dunan]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={utilStyles.headingMd}>
+        <h2>Blog</h2>
+        <ul>
+          {allPostsData.data.map((item, index) => (
+            <li className={utilStyles.listItem} key={index}>
+              {JSON.stringify(item)}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+// export async function getStaticProps() {
+//   const allPostsData = await getSortedPostsData();
+
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+export async function getServerSideProps(connect) {
+  const res = await fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
+
+  return {
+    props: {
+      allPostsData: res.json(),
+    },
+  };
 }
