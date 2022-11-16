3c745a1b07906f4dbec5ed2c01dcb0c80b2d16bb
v08nike <teddan817@gmail.com>
2022-11-16 07:05:58 -0800

    static resource managing practice

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,21 @@
 import Link from 'next/link' // client side navigation
 import Image from 'next/image'
+import Head from 'next/head'
+import Script from 'next/script'
 
 export default function FirstPost() {
   return <>
+    <Head>
+      <title>First Post</title>
+      {/* <script src="https://connect.fackbook.net/en_US/sdk.js" /> */}
+      <Script 
+        src="https://connect.fackbook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log(`script loaded correctly, window.FB has been populated!`)
+        }}
+        />
+    </Head>
     <h1>First Post</h1>
     <h2>
       <Link href="/">Back to home</Link>
